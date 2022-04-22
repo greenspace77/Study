5b17c02bc5e606a68037d281cac2435b09503908
shinwoo <turbospace@naver.com>
2022-04-22 17:41:49 +0900

    Init Commit

---
+++ textfinder.h
@@ -0,0 +1,24 @@
+#ifndef TEXTFINDER_H
+#define TEXTFINDER_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class TextFinder; }
+QT_END_NAMESPACE
+
+class TextFinder : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TextFinder(QWidget *parent = nullptr);
+    ~TextFinder();
+
+private slots:
+    void on_findButton_clicked();
+
+private:
+    Ui::TextFinder *ui;
+};
+#endif // TEXTFINDER_H
